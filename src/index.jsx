0a54a1e27cd1377e88caeba543c1019d27912838
Tibet Teixeira <tibet.teixeira@lsbd.ufc.br>
2020-11-10 23:49:05 -0300

    It finishes the calculator app

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<h1><strong>Calculator</strong></h1>
+		<Calculator />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
